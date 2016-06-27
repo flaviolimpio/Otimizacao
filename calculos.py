93967b6d0d0e44a54f4474bc212b42f5526913e7
flaviolimpio <flaviolimaoj@yahoo.com.br>
2016-06-27 18:32:04 -0300

    mudança na interface

--- calculos.py
+++ calculos.py
@@ -10,29 +10,42 @@ import math
 PI = math.pi
 
 class Calc:
+
+    ''' Classe que contem cálculos do avião'''
     
     #Asa
-    ''' Classe que contem cálculos do avião'''
-    def area(c_r, b_r, c_t, b_t):
-         return c_r*b_r+(c_r+c_t)*b_t/2
+    def area(asa):
+         return asa.c_r*asa.b_r+(asa.c_r+asa.c_t)*asa.b_t/2
      
-    def afilamento(c_t, c_r):
-         return c_t/c_r
+    def afilamento(asa):
+         return asa.c_t/asa.c_r
      
-    def alongamento(b, s):
-         return b**2/s
+    def alongamento(asa):
+         return asa.b**2/asa.s
      
-    def c_MA(c_r,afil):
-         return 2/3*c_r*((1+afil+afil**2)/(1+afil))
+    def c_MA(asa):
+         return 2/3*asa.c_r*((1+asa.tr+asa.tr**2)/(1+asa.tr))
          
-         
+    def area_molhada_asa(asa):
+        return asa.s * 2
          
     #Asas finitas
-    def delta(tr,ar):
-        ''' Calcula o delta usado '''
+    def delta(asa):
         
-        return 1# deve ser substituido por LUT (look up table)
-    
+#####################################################################################
+#####################################################################################
+#####################################################################################        
+        
+        ''' Calcula o delta usado '''
+        #Depende de tr e ar
+        return 0.03# deve ser substituido por LUT (look up table)
+        
+        
+        
+#####################################################################################
+#####################################################################################
+##################################################################################### 
+   
     def eficiencia_da_envergadura(delta):
         ''' Fator de eficiencia de envergadura (e) '''
         return 1/(1+delta)
@@ -57,10 +70,14 @@ class Calc:
         return ( (2*w/ro*s) * (k/cd0)**0.5 )**0.5
         
     def v_PrMin(v_TrMin):
-        ''' Retorna a velocidade de mínima potência requerida, também conhecidada como velocidade de máxima autom=nomia  '''
+        ''' Retorna a velocidade de mínima potência requerida, também conhecidada como velocidade de máxima autonomia  '''
         return 0.76 * v_TrMin
         
     #Polar de arrasto
+        
+    def area_molhada(aeronave):
+        return aeronave.asa.s_wet + aeronave.empenagem.s_wet + aeronave.trem_de_pouso.s_wet
+        
     def eficiencia_de_Oswald(e):
         ''' Fator de eficiência de Oswald (e0) '''
         return e * 0.75
@@ -71,8 +88,8 @@ class Calc:
         
     def coeficiente_de_atrito():
         ''' Coeficiente de atrito C_F '''
-        #Pode ser adaptado
-        return 0.0055
+        #Coeficiente de atrito equivalente C_F calculado
+        pass
         
     def coeficiente_de_arrasto_parasita(S_wet, C_F, S):
         '''Coeficiente de arrasto parasita C_D0'''
@@ -91,9 +108,9 @@ class Calc:
     def area_da_empenagem_vertical(v):
         pass
     
-    def area_molhada_asa(c_t, c_p, s):
-        ''' Área molhada, também conhecida como S_wet '''
-        return 2 * (1+ 0.2* c_t, c_p)
+#    def area_molhada_asa(c_t, c_p, s):
+#        ''' Área molhada, também conhecida como S_wet '''
+#        return 2 * (1+ 0.2* c_t, c_p)
         
     def pressao_dinamica(ro, v):
         return ( ro * v**2 )/ 2
